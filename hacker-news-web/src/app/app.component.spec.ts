c44cf6eddfa5eeb7b5a1379468c950820d07a065
amiralichesser4 <a2records.info@gmail.com>
2024-07-29 21:16:24 +0330

    services - Provide HttpClient in the spec files

--- hacker-news-web/src/app/app.component.spec.ts
+++ hacker-news-web/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        HttpClientTestingModule,
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -18,12 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hacker-news-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hacker-news-web');
   });
 });
